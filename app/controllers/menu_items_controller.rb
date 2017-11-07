6180383a594bd21d50057ea52df6e26d49be1b2a
Zakaria <zakaria.cuet@yahoo.com>
2017-11-07 19:51:42 +1100

    delayed job at all controller

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -2,7 +2,7 @@
 class MenuItemsController < ApplicationController
   def import
     uri = params[:dataset].blank? ? nil : params[:dataset][:uri]
-    raise Error::CustomError unless MenuItem.import_processig(params[:file], uri)
+    raise Error::CustomError unless MenuItem.delay.import_processig(params[:file], uri)
     redirect_to upload_url, notice: 'Menu Items imported.'
   end
 end
